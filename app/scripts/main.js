a0dd97ca277fff1b9275a4ded65adb3bac6cafc7
lorio <lori@loriortiz.com>
2016-05-15 10:09:51 -0400

    Fix for data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
 console.log('\'Allo \'Allo!');
+//function(){
+//   var fullname =artist.firstname + '-' + artist.lastname
+//    currentURL = window.location.pathname
+
+//  if (fullname === currentURL)
+//}
